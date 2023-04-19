9e1e8e01d75db0046c79a12fccbc6fe74e619db8
Valtteri Juvonen <valtteri.juvone@hotmail.com>
2023-04-19 17:56:34 +0300

    Mod to something I enjoy

--- predict.py
+++ predict.py
@@ -39,6 +39,16 @@ class Predictor(BasePredictor):
         mask: Path = Input(
             description="Black and white image to use as mask for inpainting over the image provided. White pixels are inpainted and black pixels are preserved",
         ),
+        width: int = Input(
+            description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
+            # choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
+            default=512,
+        ),
+        height: int = Input(
+            description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
+            # choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
+            default=512,
+        ),
         num_outputs: int = Input(
             description="Number of images to output. Higher number of outputs may OOM.",
             ge=1,
@@ -51,6 +61,18 @@ class Predictor(BasePredictor):
         guidance_scale: float = Input(
             description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
         ),
+        scheduler: str = Input(
+            default="DPMSolverMultistep",
+            choices=[
+                "DDIM",
+                "K_EULER",
+                "DPMSolverMultistep",
+                "K_EULER_ANCESTRAL",
+                "PNDM",
+                "KLMS",
+            ],
+            description="Choose a scheduler.",
+        ),
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
         ),
@@ -60,7 +82,13 @@ class Predictor(BasePredictor):
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
 
-        image = Image.open(image).convert("RGB").resize((512, 512))
+        if width * height > 786432:
+            raise ValueError(
+                "Maximum size is 1024x768 or 768x1024 pixels, because of memory limits. Please select a lower width or height."
+            )
+
+        # Resize image to wanted size 
+        image = Image.open(image).convert("RGB").resize((width, height))
         extra_kwargs = {
             "mask_image": Image.open(mask).convert("RGB").resize(image.size),
             "image": image
@@ -68,11 +96,16 @@ class Predictor(BasePredictor):
 
         generator = torch.Generator("cuda").manual_seed(seed)
 
+        # Make a schedualer
+        self.pipe.scheduler = make_scheduler(scheduler, pipeline.scheduler.config)
+
         output = self.pipe(
             prompt=[prompt] * num_outputs if prompt is not None else None,
             negative_prompt=[negative_prompt] * num_outputs
             if negative_prompt is not None
             else None,
+            height=height,
+            width=width,
             guidance_scale=guidance_scale,
             generator=generator,
             num_inference_steps=num_inference_steps,
@@ -86,3 +119,14 @@ class Predictor(BasePredictor):
             output_paths.append(Path(output_path))
 
         return output_paths
+
+
+def make_scheduler(name, config):
+    return {
+        "PNDM": PNDMScheduler.from_config(config),
+        "KLMS": LMSDiscreteScheduler.from_config(config),
+        "DDIM": DDIMScheduler.from_config(config),
+        "K_EULER": EulerDiscreteScheduler.from_config(config),
+        "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler.from_config(config),
+        "DPMSolverMultistep": DPMSolverMultistepScheduler.from_config(config),
+    }[name]
